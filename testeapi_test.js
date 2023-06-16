d25652a3cd738d622c93e48aad505bb602e9a985
EmyPopCorn <emillycerqueira099@gmail.com>
2023-06-16 08:38:21 -0300

    qts aula 3

--- testeapi_test.js
+++ testeapi_test.js
@@ -6,8 +6,19 @@ import {
 
 let aluno = 'Cróvis José'
 let professores = ['João', 'Laís', 'Davi', 'Bruno Aparecido Cano']
+let numeros = [11, 24, 69]
 
 Deno.test("teste de assertions (API)", () => {
     assertEquals(aluno, "Cróvis José");
     assertStringIncludes(aluno, "José");
+})
+
+Deno.test("Testando números", ()=> {
+    assertArrayIncludes(numeros, [24])
+})
+
+Deno.test("Testando array do professor (API)", ()=> {
+    assertArrayIncludes(professores,
+        ["Davi", "Lais", "Alan"],
+        "Opa! Algo deu errado");
 })
